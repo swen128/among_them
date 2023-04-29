742c9489f953c98d819b82958042e7758d52e90f
PND <fujjisaaan@gmail.com>
2023-04-29 11:00:39 +0900

    Use dedent.

--- src/word_wolf/prompt.ts
+++ src/word_wolf/prompt.ts
@@ -1,6 +1,7 @@
 import { z } from "zod";
 import { Prompt } from "../api/language_model";
 import { ChattingState, allPlayers, playerWord } from "./state";
+import dedent from "ts-dedent";
 
 export function buildPrompt(state: ChattingState): Prompt[] {
     if (state.turn.type === "human") {
@@ -14,42 +15,44 @@ export function buildPrompt(state: ChattingState): Prompt[] {
         text: message.text,
     })))
 
-    const instructions = `You are playing a game of "Word Werewolf".
+    const instructions = dedent`
+        You are playing a game of "Word Werewolf".
 
-There are ${numVillagers} villagers and 1 werewolf in the game.
-In the beginning, the players are unaware of their own roles.
+        There are ${numVillagers} villagers and 1 werewolf in the game.
+        In the beginning, the players are unaware of their own roles.
 
-Each player is assigned a secret word.
-While the villagers share the common word, the werewolf has a different one.
+        Each player is assigned a secret word.
+        While the villagers share the common word, the werewolf has a different one.
 
-Your word: "${playerWord(state, state.turn)}"
+        Your word: "${playerWord(state, state.turn)}"
 
-Players engage in conversation to figure out their own roles and identify the werewolf.
+        Players engage in conversation to figure out their own roles and identify the werewolf.
 
-After the conversation, players vote to execute someone.
-If the werewolf is executed, the villagers win; if a villager is executed, the werewolf wins.
+        After the conversation, players vote to execute someone.
+        If the werewolf is executed, the villagers win; if a villager is executed, the werewolf wins.
 
-Here are some tips:
-- Never say your secret word directly.
-- At first, give brief and vague description of the word. When the word is dog, for example, you should say something like "I adore them".
-- Ask questions about the word to find out the werewolf.
-- If you suspect you are the werewolf,
-you must blend in by deducing the villagers' word and lying to avoid detection.
-- If you think you are a villager, give others hints that you know the common word,
-while keeping the werewolf from guessing it.
+        Here are some tips:
+        - Never say your secret word directly.
+        - At first, give brief and vague description of the word. When the word is dog, for example, you should say something like "I adore them".
+        - Ask questions about the word to find out the werewolf.
+        - If you suspect you are the werewolf,
+        you must blend in by deducing the villagers' word and lying to avoid detection.
+        - If you think you are a villager, give others hints that you know the common word,
+        while keeping the werewolf from guessing it.
 
-Players: ${playerNames}
+        Players: ${playerNames}
 
-You act as ${state.turn.name}, whose character is as described below:
-${state.turn.characterDescription}
+        You act as ${state.turn.name}, whose character is as described below:
+        ${state.turn.characterDescription}
 
-Chat log: ${chatLog}
+        Chat log: ${chatLog}
 
-Your turn to say something. You must respond in the following JSON format:
-{
-    "thoughts": "Who do you think is the werewolf? Why?",
-    "say": "blah blah"
-}`;
+        Your turn to say something. You must respond in the following JSON format:
+        {
+            "thoughts": "Who do you think is the werewolf? Why?",
+            "say": "blah blah"
+        }
+    `;
 
     return [{ role: "user", content: instructions }];
 }
