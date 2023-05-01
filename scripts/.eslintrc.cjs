bc55c7c021c06a6ad14891bd0e49e906d6ec9428
PND <fujjisaaan@gmail.com>
2023-05-01 21:56:14 +0900

    [WIP] eval

---
+++ scripts/.eslintrc.cjs
@@ -0,0 +1,5 @@
+module.exports = {
+  rules: {
+    'import/no-internal-modules': 'off',
+  },
+}
---
+++ scripts/eval.ts
@@ -0,0 +1,71 @@
+import * as dotenv from 'dotenv';
+import fs from "fs";
+import yaml from "js-yaml";
+import { z } from "zod";
+import { OpenAiChat } from "../src/api";
+import { promptChat } from "../src/word_wolf/bot_brain";
+import { ChattingState, Player } from "../src/word_wolf/state";
+
+dotenv.config({ path: '.env.local' });
+const apiKey = process.env.VITE_OPENAI_API_KEY ?? "";
+const llm = new OpenAiChat(apiKey);
+
+const delay = (ms: number): Promise<void> => {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+  };
+
+async function main() {
+    const testCases = testCasesSchema.parse(yaml.load(fs.readFileSync("./scripts/data.yml", "utf-8")));
+
+    for (const testCase of testCases) {
+        evaluateWerewolfGuess(testCase).then(console.log);
+        await delay(1000);
+    }
+}
+
+const chatMessageSchema = z.string().transform(message => {
+    const messageRegex = /^\[([a-zA-Z0-9_]*)\] (.*)$/;
+    const [, senderName, text] = message.match(messageRegex) ?? [];
+    return { senderName, text };
+});
+const testCaseSchema = z.object({
+    description: z.string(),
+    commonWord: z.string(),
+    wolfWord: z.string(),
+    wolf: z.string(),
+    next: z.string(),
+    chatLog: z.array(chatMessageSchema),
+});
+const testCasesSchema = z.array(testCaseSchema);
+type TestCase = z.infer<typeof testCaseSchema>;
+
+async function evaluateWerewolfGuess(testCase: TestCase) {
+    const player = (name: string): Player => ({ type: "bot", name, characterDescription: "A confident, experienced Word Werewolf player" })
+
+    const chatLog = testCase.chatLog.map(message => {
+        return { sender: player(message.senderName), text: message.text };
+    })
+    const playerNames = [...new Set(chatLog.map(message => message.sender.name))];
+    const players = playerNames.map(player);
+
+    const chattingState: ChattingState = {
+        phase: "chat",
+        players,
+        remainingTurns: 5,
+        commonWord: testCase.commonWord,
+        wolfWord: testCase.wolfWord,
+        wolf: player(testCase.wolf),
+        chatLog,
+        turn: player(testCase.next),
+    }
+
+    const response = await promptChat(llm, chattingState);
+
+    // correct guess: +1, incorrect guess: -1, indecisive: 0
+    const score = response.suspectedWerewolf === testCase.wolf ? 1
+        : playerNames.includes(response.suspectedWerewolf) ? -1
+            : 0;
+    return { testDescription: testCase.description, score, response };
+}
+
+main().then();
