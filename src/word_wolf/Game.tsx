d857532184b01baa046d0baf127e3e98d39383d8
PND <fujjisaaan@gmail.com>
2023-05-03 17:51:19 +0900

    Improve UI.

--- src/word_wolf/Game.tsx
+++ src/word_wolf/Game.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import { useEffectOnce } from 'react-use';
 import { OpenAiChat } from '../api';
 import Chat from './Chat';
 import Vote from './Vote';
 import { useSinglePlayerGame } from './hooks';
-import { playerWord } from './state';
+import { GameState, isBot, playerWord } from './state';
 
 interface Props {
     playerName: string;
@@ -14,15 +15,24 @@ export const Game: React.FC<Props> = ({ apiKey, playerName }) => {
     const languageModel = new OpenAiChat(apiKey);
     const { state, submitChat, submitVote } = useSinglePlayerGame(languageModel, playerName);
 
+    useCheatingLog(state);
+
     return (<>
         <div className="h-screen p-4">
             <div className="w-full max-w-7xl h-full border p-4 shadow-lg rounded">
                 <div>Your word: {playerWord(state, state.humanPlayer)}</div>
-                <div>Bob's word: {playerWord(state, state.players[0])}</div>
-                <div>Alice's word: {playerWord(state, state.players[1])}</div>
                 {state.phase === "chat" && <Chat state={state} onSubmit={submitChat} />}
                 {state.phase === "vote" && <Vote state={state} onSubmit={submitVote} />}
             </div>
         </div>
     </>);
 };
+
+function useCheatingLog(state: GameState) {
+    useEffectOnce(() => {
+        const botPlayers = state.players.filter(isBot);
+        botPlayers.forEach(player => {
+            console.log(`${player.name}'s word: ${playerWord(state, player)}`);
+        });
+    });
+}
