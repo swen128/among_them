f8d010791e75efec1256feebb0deb99dad7c0cab
PND <fujjisaaan@gmail.com>
2023-05-04 13:44:01 +0900

    Disable the chat input while the bot is thinking.

--- src/word_wolf/Chat.tsx
+++ src/word_wolf/Chat.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import ChatLog from './ChatLog';
-import { ChattingState } from './state';
+import { ChattingState, isPlayerTurn } from './state';
 
 interface Props {
     state: ChattingState;
@@ -9,6 +9,7 @@ interface Props {
 
 const Chat: React.FC<Props> = ({ state, onSubmit }) => {
     const [inputValue, setInputValue] = useState('');
+    const canSubmit = isPlayerTurn(state);
 
     const handleSubmit = (e: React.FormEvent) => {
         e.preventDefault();
@@ -24,9 +25,11 @@ const Chat: React.FC<Props> = ({ state, onSubmit }) => {
                 <input
                     className="focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-2 px-4 block appearance-none leading-normal w-full"
                     type="text"
-                    placeholder="Type your message..."
+                    placeholder={canSubmit ? "Type your message..." : "The bot is thinking..."}
                     value={inputValue}
                     onChange={(e) => setInputValue(e.target.value)}
+                    disabled={!canSubmit}
+                    autoFocus
                 />
             </form>
         </div>
